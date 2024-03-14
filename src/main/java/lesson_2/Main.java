f20355d49f2dc7edea4a0a727828ead1683e9bd5
Natalya Shipova <nataliefrance@mail.ru>
2024-03-14 10:04:38 +0300

    Initial commit

---
+++ src/main/java/lesson_2/Main.java
@@ -0,0 +1,60 @@
+package lesson_2;
+
+import java.sql.*;
+
+public class Main {
+    private static Connection connection;
+    private static Statement stmt;
+
+
+    public static void main(String[] args) {
+        try {
+            connect();
+            printTable();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void connect() throws ClassNotFoundException, SQLException {
+        //Class.forName("org.sqlite.JDBC");
+        //DriverManager – Менеджер драйверов, управляющий списком драйверов БД и позволяющий открыть соединение с базой данных
+        //Метод getConnection на основании параметра URL находит java.sql.Driver соответствующей базы данных и вызывает у него метод connect.
+        connection = DriverManager.getConnection("jdbc:sqlite:usersDB");
+        //stmt всегда возвращает таблицу
+        stmt = connection.createStatement();
+    }
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createDB() {
+
+    }
+
+    public static void printTable() throws SQLException {
+        //Экземпляры этого типа содержат данные, которые были получены в результате выполнения SELECT запроса
+        ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+
+        while(rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString("name") + " " + rs.getString("score"));
+        }
+    }
+
+    public static void printMetaData() throws SQLException {
+        ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+        //из ResultSet достаём метаданные о состоянии таблицы.
+        ResultSetMetaData resultSetMetaData = rs.getMetaData();
+
+        for (int i = 1; i < resultSetMetaData.getColumnCount(); i++) {
+            System.out.println(resultSetMetaData.getColumnName(i));
+        }
+    }
+}
