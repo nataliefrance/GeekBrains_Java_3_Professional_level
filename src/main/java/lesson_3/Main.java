159a19ad66a672e6eee6c6286b5b49706dd89dd1
Natalya Shipova <nataliefrance@mail.ru>
2024-03-18 09:49:11 +0300

    Домашнее задание по работе с java.io. Добавлен метод concatFiles, который считывает несколько файлов и записывает их в один.

--- src/main/java/lesson_3/Main.java
+++ src/main/java/lesson_3/Main.java
@@ -2,6 +2,8 @@ package lesson_3;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
@@ -9,6 +11,8 @@ public class Main {
         //1. Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль;
         System.out.println(Arrays.toString(fileToArray("F:\\Natasha\\Repo\\GeekBrains\\GeekBrains_Java_3_Professional_level\\src\\main\\java\\lesson_3\\1.txt")));
 
+        //2. Последовательно сшить 5 файлов в один (файлы примерно 100 байт).
+        concatFiles();
 
     }
 
@@ -22,5 +26,24 @@ public class Main {
             }
             return outputStream.toByteArray();
         }
+    }
+
+    public static void concatFiles() throws IOException {
+        List<FileInputStream> listOfFiles = Arrays.asList(
+                new FileInputStream("src/main/java/lesson_3/2_1.txt"),
+                new FileInputStream("src/main/java/lesson_3/2_2.txt"),
+                new FileInputStream("src/main/java/lesson_3/2_3.txt"),
+                new FileInputStream("src/main/java/lesson_3/2_4.txt"),
+                new FileInputStream("src/main/java/lesson_3/2_5.txt")
+        );
+
+        try (SequenceInputStream sis = new SequenceInputStream(Collections.enumeration(listOfFiles));
+             FileOutputStream fos = new FileOutputStream("src/main/java/lesson_3/2_result.txt")) {
+            int x;
+            while ((x = sis.read()) != -1) {
+                fos.write(x);
+            }
+        }
+
     }
 }
